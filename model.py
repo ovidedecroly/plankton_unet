6acc1ae65068d01a2bff2d1a3ea0a1e21b0c2097
Ovide Academy <ovide@staff.uns.ac.id>
2023-07-19 05:49:36 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import segmentation_models_pytorch as smp
 
-def get_unet_model(name='efficientnet-b3', in_channels=3, classes=21, activation=None):
+def get_unet_model(name='efficientnet-b3', in_channels=3, classes=1, activation=None):
     model = smp.Unet(encoder_name=name, in_channels=in_channels, classes=classes, activation=activation)
     return model
